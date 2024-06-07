1777af79a9bc81d648c80f57307ab545c7e1a069
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2024-06-07 16:07:12 -0300

    Update

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,6 +1,5 @@
-// scripts.js
 document.addEventListener('DOMContentLoaded', function() {
-    var button1 = document.querySelector('#box1 button');
+    var button1 = document.querySelector('#history .box button');
     button1.addEventListener('click', function() {
         var additionalContent = document.createElement('p');
         additionalContent.innerText = 'Tibia oferece um vasto mundo com inúmeras quests, perigos e aventuras!';
@@ -20,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    var box3 = document.getElementById('box3');
+    var box3 = document.querySelector('#tips .box');
     box3.addEventListener('click', function() {
         alert('Explore mais dicas em nosso blog para melhorar seu jogo em Tibia!');
     });
